05d3d88d35b669c667267e668ea7fb0910529adc
jacksuh <jacksonsdss@gmail.com>
2023-02-18 00:04:19 -0300

    Testes

---
+++ src/test/java/med/voll/api/medico/MedicoTest.java
@@ -0,0 +1,19 @@
+package med.voll.api.medico;
+
+
+/**
+ * Classe MedicoTest, responsavel por Testar das regras Salvar,Listar,Atualizar,Deletar .
+ * Autor: Jackson Santos
+ */
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MedicoTest {
+
+
+
+}
